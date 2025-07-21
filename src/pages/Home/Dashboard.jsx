494da44789d191fff62cb7338931681293fcb8f5
agprateek7 <agprateek7@gmai.com>
2025-07-21 17:13:17 +0530

    Code Editing

--- src/pages/Home/Dashboard.jsx
+++ src/pages/Home/Dashboard.jsx
@@ -56,13 +56,6 @@ const Dashboard = () => {
     <div className="min-h-screen bg-[#0f172a] text-white px-4 md:px-8 py-6">
       <div className="flex justify-between items-center mb-6">
         <h2 className="text-2xl font-bold text-white">Your Sessions</h2>
-        {/* <button
-          className="flex items-center gap-2 bg-blue-700 hover:bg-blue-600 text-white font-semibold px-5 py-2.5 rounded-full shadow-md transition"
-          onClick={() => setOpenCreateModal(true)}
-        >
-          <LuPlus className="text-xl" />
-          Add New
-        </button> */}
       </div>
 
       {sessions?.length === 0 ? (
@@ -118,7 +111,7 @@ const Dashboard = () => {
             onDelete={() => deleteSession(openDeleteAlert.data)}
           />
         </div>
-      </Modal>
+      </Modal>  
     </div>
   </DashboardLayout>
 );
